936feca6e16699e75bffe11873dfaf1c38a15026
lni102 <lukaas.nilsen@gmail.com>
2024-12-02 16:04:41 +0100

    changed arguments in the functions
    takes list of images to return augmented image list

--- transformation.py
+++ transformation.py
@@ -1,28 +1,24 @@
-import os 
 import albumentations as A
 import numpy as np
 from random import randint
 import cv2
 
-
-def resize_img(image, shape, p=1.0):
+def resize_img(image, shape):
     x, y = shape
-    resize = A.Resize(height=x, width=y, p=p)  # pass height and width separately
-
+    resize = A.Resize(height=x, width=y, p=0.1)  # pass height and width separately
     augmented = resize(image=image)
     return augmented['image']
 
 # compression function based on image type (assuming only JPEG or PNG)
-def simulate_compression(image, image_type, compress_to_jpeg=False):
-    if image_type in ['.jpg', '.jpeg'] or compress_to_jpeg:
+def simulate_compression(image, quality_range, compress_to_jpeg=True):
+    if compress_to_jpeg:
         # JPEG compression (lossy) 
-        compress = A.ImageCompression(quality_range=(95,95), compression_type='jpeg', p=1.0) # compression quality of 95%
-    elif image_type == '.png':
+        compress = A.ImageCompression(quality_range, compression_type='jpeg', p=1.0) # compression quality of 95%
+    else:
         # PNG compression (lossless) - using simulated lossless webp compression 
-        compress = A.ImageCompression(quality_range=(95,95), compression_type='webp', p=1.0)  
+        compress = A.ImageCompression(quality_range, compression_type='webp', p=1.0)  
     augmented = compress(image=image)
     return augmented['image']
-  
 
 def apply_mask(image, mask_color=(0, 0, 0), precentage_masking = 0.1):
     h, w, _ = image.shape
@@ -36,20 +32,18 @@ def apply_mask(image, mask_color=(0, 0, 0), precentage_masking = 0.1):
     masked_image = cv2.bitwise_and(image, mask)   
     return masked_image
 
-def transform_images(image_list, image_paths, target_size=None, compression=False, compress_to_jpeg = False, mask = False):
+
+def transform_images(image_list, quality_range = 95, target_size = None, compression = True, compress_to_jpeg = True, mask = False):
     transformed_images = []
     # apply all transformations individually for each image
-    for img, img_path in zip(image_list, image_paths):
-        _, ext = os.path.splitext(img_path) # check the file extension to determine the image type
-        ext = ext.lower() 
-
+    for img in image_list:
         transformed_image = img # original image
 
         if target_size: 
             transformed_image = resize_img(transformed_image, target_size)  
 
         if compression: 
-            transformed_image = simulate_compression(transformed_image, ext, compress_to_jpeg)
+            transformed_image = simulate_compression(transformed_image, quality_range, compress_to_jpeg)
 
         if mask:
             transformed_image = apply_mask(transformed_image)
