97e171c783ee51baff74685be21a09af08e14b4a
Dabrowka <dkn002@uit.no>
2024-12-02 16:34:10 +0100

    updated to take paths as input

--- psnr_pathwise.py
+++ psnr_pathwise.py
@@ -14,27 +14,6 @@ from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.transform import resize
 
 
-def psnr_calculation(transformed_list, original_list):
-
-    
-    img = transformed_list                # Transformed input image
-    org = original_list                   # Original input image (not transformed)
-    psnr_values = []                      # To store PSNR values at each iteration
-
-    # Compute PSNR for each image
-    for j in range(len(img)):
-        # Resize original image to match transformed image shape
-        reshaped_original = resize(org[j], img[j].shape, anti_aliasing=True, preserve_range=True)
-
-        # Calculate PSNR
-        psnr_value = psnr(reshaped_original, img[j], data_range=reshaped_original.max() - reshaped_original.min())
-        psnr_values.append(psnr_value)
-    
-  
-    return psnr_values
-
-
-
 def psnr_calculation(transformed_path, original_path):
     """
     Calculate PSNR values between transformed and original images.
@@ -66,5 +45,6 @@ def psnr_calculation(transformed_path, original_path):
         psnr_values.append(psnr_value)
 
     return psnr_values
+
 
 
